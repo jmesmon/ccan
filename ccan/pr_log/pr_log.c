9bc8b741eec24bf58514c377f4a82d672df0d450
Cody P Schafer <devel@codyps.com>
2013-12-31 20:52:47 -0800

    pr_log: add initial
    A simple printf logging infra where levels are detemined by the
    value of the "DEBUG" environment variable.
    
    This is loosely based on the interfaces & functionality of Linux's
    printk() and pr_*() wrapper macros.

---
+++ ccan/pr_log/pr_log.c
@@ -0,0 +1,64 @@
+#include "pr_log.h"
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define DEBUG_NEED_INIT INT_MIN
+static int debug = DEBUG_NEED_INIT;
+
+bool debug_is(int lvl)
+{
+	return lvl <= debug_level();
+}
+
+int debug_level(void)
+{
+	if (debug != DEBUG_NEED_INIT)
+		return debug;
+	char *c = getenv("DEBUG");
+	if (!c) {
+		debug = 0;
+		return 0;
+	}
+
+	debug = atoi(c);
+	return debug;
+}
+
+static int map_char_to_level(char i)
+{
+	switch (i) {
+# define DL(name, ch, lvl)	\
+	case ch:		\
+		return lvl;
+# include "pr_log_levels.def"
+# undef DL
+	default:
+		return INT_MIN;
+	}
+}
+
+void pr_log(char const fmt[static 1], ...)
+{
+	int level = INT_MIN;
+	if (fmt[0] == '-' && isdigit(fmt[1]) && fmt[2] == ':')
+		level = - (fmt[1] - '0');
+	else if (fmt[0] && fmt[1] == ':') {
+		if (isdigit(fmt[0]))
+			level = fmt[0] - '0';
+		else
+			level = map_char_to_level(fmt[0]);
+	}
+
+	if (!debug_is(level))
+		return;
+
+	va_list va;
+	va_start(va, fmt);
+	vfprintf(stderr, fmt, va);
+	va_end(va);
+}
---
+++ ccan/pr_log/pr_log.h
@@ -0,0 +1,49 @@
+#ifndef CCAN_DEBUG_H_
+#define CCAN_DEBUG_H_
+
+#include <stdbool.h>
+#include <ccan/compiler/compiler.h>
+
+#ifdef NDEBUG
+static PRINTF_FMT(1,2) inline void pr_log(char const *fmt, ...)
+{
+	(void)fmt;
+}
+static inline bool debug_is(int lvl) { (void)lvl; return false; }
+static inline int debug_level(void) { return 0; }
+#else
+void PRINTF_FMT(1,2) pr_log(char const *fmt, ...);
+bool debug_is(int lvl);
+int debug_level(void);
+#endif
+
+/* TODO: generate the following defines somehow */
+#define LOG_EMERG "-4:"
+#define LOG_ALERT "-3:"
+#define LOG_CRIT  "-2:"
+#define LOG_ERROR "-1:"
+#define LOG_WARN   "0:"
+#define LOG_NOTICE "1:"
+#define LOG_INFO   "2:"
+#define LOG_DEBUG  "3:"
+
+#define pr_emerg(...)  pr_log(LOG_EMERG  __VA_ARGS__)
+#define pr_alert(...)  pr_log(LOG_ALERT  __VA_ARGS__)
+#define pr_crit(...)   pr_log(LOG_CRIT   __VA_ARGS__)
+#define pr_error(...)  pr_log(LOG_ERROR  __VA_ARGS__)
+#define pr_warn(...)   pr_log(LOG_WARN   __VA_ARGS__)
+#define pr_notice(...) pr_log(LOG_NOTICE __VA_ARGS__)
+#define pr_info(...)   pr_log(LOG_INFO   __VA_ARGS__)
+#define pr_debug(...)  pr_log(LOG_DEBUG  __VA_ARGS__)
+
+#ifdef DEBUG
+# define pr_devel(...)  pr_log("-9:" __VA_ARGS__)
+#else
+static PRINTF_FMT(1,2) inline void pr_check_printf_args(const char *fmt, ...)
+{
+	(void)fmt;
+}
+# define pr_devel(...) pr_check_printf_args(__VA_ARGS__)
+#endif
+
+#endif
