1a19880057c225ed2b1c0d3b30ded580c11193c3
Cody P Schafer <dev@codyps.com>
2016-06-22 16:01:26 -0400

    htable/htable_type: provide seperate macros for types & fns
    Allows one to build structures that refer to themselves.
    
    Ie: one can define a struct Foo with contains a htable of pointers to
    struct Foo.
    
    Signed-off-by: Cody P Schafer <dev@codyps.com>

--- ccan/htable/htable_type.h
+++ ccan/htable/htable_type.h
@@ -5,56 +5,11 @@
 #include <ccan/compiler/compiler.h>
 #include "config.h"
 
-/**
- * HTABLE_DEFINE_TYPE - create a set of htable ops for a type
- * @type: a type whose pointers will be values in the hash.
- * @keyof: a function/macro to extract a key: <keytype> @keyof(const type *elem)
- * @hashfn: a hash function for a @key: size_t @hashfn(const <keytype> *)
- * @eqfn: an equality function keys: bool @eqfn(const type *, const <keytype> *)
- * @prefix: a prefix for all the functions to define (of form <name>_*)
- *
- * NULL values may not be placed into the hash table.
- *
- * This defines the type hashtable type and an iterator type:
- *	struct <name>;
- *	struct <name>_iter;
- *
- * It also defines initialization and freeing functions:
- *	void <name>_init(struct <name> *);
- *	bool <name>_init_sized(struct <name> *, size_t);
- *	void <name>_clear(struct <name> *);
- *	bool <name>_copy(struct <name> *dst, const struct <name> *src);
- *
- * Add function only fails if we run out of memory:
- *	bool <name>_add(struct <name> *ht, const <type> *e);
- *
- * Delete and delete-by key return true if it was in the set:
- *	bool <name>_del(struct <name> *ht, const <type> *e);
- *	bool <name>_delkey(struct <name> *ht, const <keytype> *k);
- *
- * Find and return the (first) matching element, or NULL:
- *	type *<name>_get(const struct @name *ht, const <keytype> *k);
- *
- * Find and return all matching elements, or NULL:
- *	type *<name>_getfirst(const struct @name *ht, const <keytype> *k,
- *			      struct <name>_iter *i);
- *	type *<name>_getnext(const struct @name *ht, const <keytype> *k,
- *			     struct <name>_iter *i);
- *
- * Iteration over hashtable is also supported:
- *	type *<name>_first(const struct <name> *ht, struct <name>_iter *i);
- *	type *<name>_next(const struct <name> *ht, struct <name>_iter *i);
- *	type *<name>_prev(const struct <name> *ht, struct <name>_iter *i);
- *
- * It's currently safe to iterate over a changing hashtable, but you might
- * miss an element.  Iteration isn't very efficient, either.
- *
- * You can use HTABLE_INITIALIZER like so:
- *	struct <name> ht = { HTABLE_INITIALIZER(ht.raw, <name>_hash, NULL) };
- */
-#define HTABLE_DEFINE_TYPE(type, keyof, hashfn, eqfn, name)		\
+#define HTABLE_DEFINE_TYPE_STRUCT(name)					\
 	struct name { struct htable raw; };				\
-	struct name##_iter { struct htable_iter i; };			\
+	struct name##_iter { struct htable_iter i; };
+
+#define HTABLE_DEFINE_TYPE_FUNCS(type, keyof, hashfn, eqfn, name)	\
 	static inline size_t name##_hash(const void *elem, void *priv)	\
 	{								\
 		(void)priv;						\
@@ -154,6 +109,65 @@
 	{								\
 		return htable_prev(&ht->raw, &iter->i);			\
 	}
+
+/**
+ * HTABLE_DEFINE_TYPE - create a set of htable ops for a type
+ * @type: a type whose pointers will be values in the hash.
+ * @keyof: a function/macro to extract a key: <keytype> @keyof(const type *elem)
+ * @hashfn: a hash function for a @key: size_t @hashfn(const <keytype> *)
+ * @eqfn: an equality function keys: bool @eqfn(const type *, const <keytype> *)
+ * @prefix: a prefix for all the functions to define (of form <name>_*)
+ *
+ * NULL values may not be placed into the hash table.
+ *
+ * This defines the type hashtable type and an iterator type:
+ *	struct <name>;
+ *	struct <name>_iter;
+ *
+ * It also defines initialization and freeing functions:
+ *	void <name>_init(struct <name> *);
+ *	bool <name>_init_sized(struct <name> *, size_t);
+ *	void <name>_clear(struct <name> *);
+ *	bool <name>_copy(struct <name> *dst, const struct <name> *src);
+ *
+ * Add function only fails if we run out of memory:
+ *	bool <name>_add(struct <name> *ht, const <type> *e);
+ *
+ * Delete and delete-by key return true if it was in the set:
+ *	bool <name>_del(struct <name> *ht, const <type> *e);
+ *	bool <name>_delkey(struct <name> *ht, const <keytype> *k);
+ *
+ * Find and return the (first) matching element, or NULL:
+ *	type *<name>_get(const struct @name *ht, const <keytype> *k);
+ *
+ * Find and return all matching elements, or NULL:
+ *	type *<name>_getfirst(const struct @name *ht, const <keytype> *k,
+ *			      struct <name>_iter *i);
+ *	type *<name>_getnext(const struct @name *ht, const <keytype> *k,
+ *			     struct <name>_iter *i);
+ *
+ * Iteration over hashtable is also supported:
+ *	type *<name>_first(const struct <name> *ht, struct <name>_iter *i);
+ *	type *<name>_next(const struct <name> *ht, struct <name>_iter *i);
+ *	type *<name>_prev(const struct <name> *ht, struct <name>_iter *i);
+ *
+ * It's currently safe to iterate over a changing hashtable, but you might
+ * miss an element.  Iteration isn't very efficient, either.
+ *
+ * You can use HTABLE_INITIALIZER like so:
+ *	struct <name> ht = { HTABLE_INITIALIZER(ht.raw, <name>_hash, NULL) };
+ */
+#define HTABLE_DEFINE_TYPE(type, keyof, hashfn, eqfn, name)		\
+	HTABLE_DEFINE_TYPE_STRUCT(name)					\
+	HTABLE_DEFINE_TYPE_FUNCS(type, keyof, hashfn, eqfn, name)
+
+/**
+ * HTABLE_INIT - initializer for a htable defined with HTABLE_DEFINE_TYPE
+ * @self_: an expression that resolves to the htable being initialized
+ * @name_: the name of the htable as given to HTABLE_DEFINE_TYPE
+ *
+ */
+#define HTABLE_INIT(self_, name_) { HTABLE_INITIALIZER(self_.raw, name_##_hash, NULL) }
 
 #if HAVE_TYPEOF
 #define HTABLE_KTYPE(keyof, type) typeof(keyof((const type *)NULL))
